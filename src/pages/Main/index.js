2ff8899a50d265ce6ed4ea4ec0c1a24663fe6dcb
davimattos <dmattos340@gmail.com>
2019-06-08 18:47:23 -0300

    preparing card to translate in radius X

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -9,18 +9,29 @@ import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
 
 import {
-  Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation,
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
 } from './styles';
 
 export default function Main() {
-  let offset = 0;
+  let offsetY = 0;
+  let offsetX = 0;
   const translateY = new Animated.Value(0);
+  const translateX = new Animated.Value(0);
 
   const animatedEvent = Animated.event(
     [
       {
         nativeEvent: {
           translationY: translateY,
+          translationX: translateX,
         },
       },
     ],
@@ -31,15 +42,24 @@ export default function Main() {
     if (event.nativeEvent.oldState === State.ACTIVE) {
       let opened = false;
       const { translationY } = event.nativeEvent;
+      const { translationX } = event.nativeEvent;
 
-      offset += translationY;
+      offsetY += translationY;
+      offsetX += translationX;
 
       if (translationY >= 100) {
         opened = true;
       } else {
-        translateY.setOffset(offset);
+        translateY.setOffset(offsetY);
         translateY.setValue(0);
-        offset = 0;
+        offsetY = 0;
+      }
+      if (translationX >= 100) {
+        opened = true;
+      } else {
+        translateX.setOffset(offsetY);
+        translateX.setValue(0);
+        offsetY = 0;
       }
 
       Animated.timing(translateY, {
@@ -47,9 +67,19 @@ export default function Main() {
         duration: 200,
         useNativeDriver: true,
       }).start(() => {
-        offset = opened ? 380 : 0;
-        translateY.setOffset(offset);
+        offsetY = opened ? 380 : 0;
+        translateY.setOffset(offsetY);
         translateY.setValue(0);
+      });
+
+      Animated.timing(translateX, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offsetX = opened ? 380 : 0;
+        translateX.setOffset(offsetX);
+        translateX.setValue(0);
       });
     }
   }
@@ -65,15 +95,25 @@ export default function Main() {
           onGestureEvent={animatedEvent}
           onHandlerStateChange={onHandlerStateChanged}
         >
-          <Card style={{
-            transform: [{
-              translateY: translateY.interpolate({
-                inputRange: [-200, 0, 380],
-                outputRange: [-50, 0, 380],
-                extrapolate: 'clamp',
-              }),
-            }],
-          }}
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-200, 0, 380],
+                    outputRange: [0, 0, 380],
+                    extrapolate: 'clamp',
+                  }),
+                },
+                {
+                  translateX: translateX.interpolate({
+                    inputRange: [-400, 0, 100],
+                    outputRange: [-400, 0, 0],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}
           >
             <CardHeader>
               <Icon name="attach-money" size={28} color="#666" />
@@ -84,13 +124,10 @@ export default function Main() {
               <Description>R$ 27.897,09</Description>
             </CardContent>
             <CardFooter>
-              <Annotation>
-                Transferência de R$ 10,00 recebida hoje
-              </Annotation>
+              <Annotation>Transferência de R$ 10,00 recebida hoje</Annotation>
             </CardFooter>
           </Card>
         </PanGestureHandler>
-
       </Content>
 
       <Tabs translateY={translateY} />
