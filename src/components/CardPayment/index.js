1ba01c1a4dcf33e286a20aa87cacf5823b9ff5a5
davimattos <dmattos340@gmail.com>
2019-06-10 00:25:52 -0300

    create payment card

--- src/components/CardPayment/index.js
+++ src/components/CardPayment/index.js
@@ -4,26 +4,49 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {
   Card,
+  CardContainer,
   CardHeader,
+  CardIcon,
   CardContent,
   CardFooter,
+  CardStatus,
   Annotation,
   Title,
+  Date,
   Description,
+  NextInvoice,
+  CurrentInvoice,
+  AvailableLimit,
+  Button,
 } from './styles';
 
 export default function CardPayment() {
   return (
     <Card>
-      <CardHeader>
-        <Icon />
-      </CardHeader>
-      <CardContent>
-        <Title />
-        <Description />
-      </CardContent>
+      <CardContainer>
+        <CardHeader>
+          <CardIcon>
+            <Icon name="payment" size={30} color="#666" />
+          </CardIcon>
+          <CardContent>
+            <Title>FATURA FECHADA</Title>
+            <Description>R$ 34.567,87</Description>
+            <Date>VENCIMENTO 25 MAI</Date>
+            <Button>
+              <Title>PAGAR</Title>
+            </Button>
+          </CardContent>
+        </CardHeader>
+        <CardStatus>
+          <NextInvoice />
+          <CurrentInvoice />
+          <AvailableLimit />
+        </CardStatus>
+      </CardContainer>
       <CardFooter>
-        <Annotation />
+        <Icon name="palette" size={28} color="#666" />
+        <Annotation>Compra mais recente em Empório Gourmet no valor de R$ 122,89 hoje</Annotation>
+        <Icon name="replay" size={28} color="#666" />
       </CardFooter>
     </Card>
   );
--- src/components/CardPayment/styles.js
+++ src/components/CardPayment/styles.js
@@ -1,46 +1,104 @@
 import styled from 'styled-components/native';
+import { StyleSheet } from 'react-native';
 
 export const Card = styled.View`
   height: 350;
   width: 380;
   background-color: #fff;
   margin: 10px;
-  border-radius: 3px;
+  border-radius: 2px;
+`;
+
+export const CardContainer = styled.View`
+  flex: 5;
+  flex-direction: row;
 `;
 
 export const CardHeader = styled.View`
+  flex: 8;
+`;
+
+export const CardIcon = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  margin: 20px 20px;
+  margin: 20px 30px;
+`;
+
+export const CardStatus = styled.View`
+  flex: 2;
+  align-items: center;
+`;
+
+export const NextInvoice = styled.View`
+  flex: 1;
+  width: 10px;
+  background-color: #f38723;
+  margin-top: 20px;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+`;
+
+export const CurrentInvoice = styled.View`
+  flex: 2;
+  width: 10px;
+  background-color: #d13f4a;
+`;
+
+export const AvailableLimit = styled.View`
+  flex: 6;
+  width: 10px;
+  background-color: #2dd600;
+  margin-bottom: 20px;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
 `;
 
 export const CardContent = styled.View`
   flex: 1;
   padding: 0 30px;
-  justify-content: center;
+  margin-top: 10px;
 `;
 
 export const CardFooter = styled.View`
-  flex-direction: row;
+  flex: 1;
   justify-content: space-between;
+  flex-direction: row;
   align-items: center;
-  padding: 30px;
+  padding: 20px 25px;
   background: #eee;
-  border-radius: 4px;
+  border-radius: 2px;
 `;
 
 export const Title = styled.Text`
   font-size: 13px;
-  color: #999;
+  color: #d13f4a;
+  font-weight: 500;
 `;
 
 export const Description = styled.Text`
   font-size: 32px;
-  margin-top: 3px;
-  color: #333;
+  color: #d13f4a;
+  font-weight: 600;
+`;
+
+export const Date = styled.Text`
+  font-size: 13px;
+  color: #999;
+  font-weight: 300;
+`;
+
+export const Button = styled.TouchableOpacity`
+  align-self: flex-start;
+  border-color: #d13f4a;
+  border-width: ${StyleSheet.hairlineWidth}px;
+  border-radius: 2px;
+  margin: 10px 1px;
+  padding: 5px 40px;
 `;
 
 export const Annotation = styled.Text`
   font-size: 12px;
   color: #333;
+  width: 75%;
+  margin: 0 5px;
 `;
